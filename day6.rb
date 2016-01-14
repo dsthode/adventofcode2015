f8f1b0ec0cece19925c7479303acc3012a53ad19
Damian Serrano Thode <dsthode@gmail.com>
2016-01-14 19:05:48 +0100

    content

--- day6.rb
+++ day6.rb
@@ -9,28 +9,35 @@ def get_pairs(x1, y1, x2, y2)
 end
 
 input = File.read(ARGV[0])
-display = Array.new(1000) { Array.new(1000, false) }
+display = Array.new(1000) { Array.new(1000, 0) }
 turn_on_off_regex = /turn (on|off) (\d{0,3}),(\d{0,3}) through (\d{0,3}),(\d{0,3})/
 toggle_regex = /toggle (\d{0,3}),(\d{0,3}) through (\d{0,3}),(\d{0,3})/
-count = 0
 input.lines.each do |line|
 	if matches = turn_on_off_regex.match(line)
 		pairs = get_pairs(matches[2], matches[3], matches[4], matches[5])
 		pairs.each do |pair|
-			display[pair[0], pair[1]] = (matches[1] == 'on') ? true : false
+			if matches[1] == 'on'
+				display[pair[0]][pair[1]] += 1
+			else
+				display[pair[0]][pair[1]] -= 1 if display[pair[0]][pair[1]] > 0
+			end
 		end
 	elsif matches = toggle_regex.match(line)
 		pairs = get_pairs(matches[1], matches[2], matches[3], matches[4])
 		pairs.each do |pair|
-			display[pair[0], pair[1]] = !display[pair[0], pair[1]]
+			display[pair[0]][pair[1]] += 2
 		end
+	else
+		puts 'No match!'
+		exit
 	end
 end
 
+count = 0
 (0..999).each do |x|
 	(0..999).each do |y|
-		count += 1 if display[x, y]
-	end
+		count += display[x][y]
+	 end
 end
 
-puts "Total lights on: #{count}"
+puts "Total intensity: #{count}"
